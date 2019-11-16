7de2392e3fd125842fb1735227e20a72c7d87bf2
Jérémy Khouri <39912632+jerkhouri@users.noreply.github.com>
2019-11-16 17:39:05 +0100

    DELETE
    Suppression du fichier qui servait de test

--- python/testreading.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-import csv
-
-with open('data/data.csv', newline='') as csvfile:
-	spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-	for row in spamreader:
-		print(', '.join(row))
